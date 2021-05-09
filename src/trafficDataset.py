5db5d7bed20650e82fd03306e9164342c6b1a9fa
aprlie <sylover@pku.edu.cn>
2021-05-09 14:31:39 +0000

    update_dataset_file

--- src/trafficDataset.py
+++ src/trafficDataset.py
@@ -7,12 +7,9 @@ class TrafficDataset(Dataset):
         if mode == 'default':
             self.x = inputs[split+'_x'] # [T, Len, N]
             self.y = inputs[split+'_y'] # [T, Len, N]
-            # self.edge_index = inputs['edge_index'] # [2, N]
         else:
             self.x = x
             self.y = y
-            # self.edge_index = edge_index.numpy()
-        # print(self.x.shape, self.y.shape, self.edge_index.shape)
     
     def __len__(self):
         return self.x.shape[0]
@@ -20,19 +17,15 @@ class TrafficDataset(Dataset):
     def __getitem__(self, index):
         x = torch.Tensor(self.x[index].T)
         y = torch.Tensor(self.y[index].T)
-        # edge_index = torch.Tensor(self.edge_index).to(torch.long)
-        return Data(x=x, y=y)#, edge_index=edge_index)   
+        return Data(x=x, y=y)  
     
 class continue_learning_Dataset(Dataset):
-    def __init__(self, inputs):#, graph):
+    def __init__(self, inputs):
         self.x = inputs # [T, Len, N]
-        # self.edge_index = graph # [N, N]
-        # print(self.x.shape, self.y.shape, self.edge_index.shape)
     
     def __len__(self):
         return self.x.shape[0]
 
     def __getitem__(self, index):
         x = torch.Tensor(self.x[index].T)
-        # edge_index = torch.Tensor(self.edge_index).to(torch.long)
-        return Data(x=x)#, edge_index=edge_index)
+        return Data(x=x)
